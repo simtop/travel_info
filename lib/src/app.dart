e7072064a3c3491ac9e00627512dd30471603a21
Simon <stopchyan@bemobile.es>
2019-12-13 13:36:20 +0100

    Color fix

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,7 +7,9 @@ class Demo extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData.light(),
+      theme: ThemeData(
+        primaryColor : Colors.blue
+    ),
       darkTheme: ThemeData.dark(),
       home: HomePage(title: 'Flutter Demo'),
     );
